a6c1e2249b6a3eb219bbad9c27da9627e6a88929
Sophia Edeki <sophiesse143@gmail.com>
2022-12-14 08:11:57 +0100

    updated

--- fundusai/src/App.js
+++ fundusai/src/App.js
@@ -4,15 +4,24 @@ import About from './pages/About';
 import Services from './pages/Services';
 import Contact from './pages/Contact';
 import Error from './pages/Error';
+import worksData from './components/worksData';
 import SharedLayout from './pages/SharedLayout';
 
 function App() {
+  const worksElements = worksData.map(item => {
+    return ( 
+    <Services
+        key={item.id}
+        {...item}
+    />
+    )
+  })
   return (
       <BrowserRouter>
       <Routes>
           <Route path='/' element= {<SharedLayout/>}>
           <Route index element= {<Home />}/>
-          <Route path='services' element= {<Services />}/>
+          <Route path='services' element= {worksElements}/>
           <Route path='about' element= {<About />}/>
           <Route path='contact' element= {<Contact />}/>
           <Route path='*' element= {<Error />}/>
---
+++ fundusai/src/components/worksData.js
@@ -0,0 +1,35 @@
+var worksData = [
+    {
+        id: 1,
+        title: "Capture retina image",
+        num: 1,
+        img: "how-it-works.jpg",
+        listOne:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        listTwo:"Quam duis viverra amet, ultrices risus quis integer nec nulla.",
+        listThree:"Tortor turpis nibh viverra nunc bibendum.",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra est purus eu tempor tincidunt commodo.",
+    },
+    {
+        id: 2,
+        title: "Tortor turpis nibh viverra nunc bibendum.",
+        num: 2,
+        img: "how-it-works2.jpg",
+        listOne:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        listTwo:"Quam duis viverra amet, ultrices risus quis integer nec nulla.",
+        listThree:"Tortor turpis nibh viverra nunc bibendum.",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra est purus eu tempor tincidunt commodo.",
+    },
+    {
+        id: 3,
+        title: "Diagnosis",
+        num: 3,
+        img: "how-it-works3.jpg",
+        listOne:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        listTwo:"Quam duis viverra amet, ultrices risus quis integer nec nulla.",
+        listThree:"Tortor turpis nibh viverra nunc bibendum.",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra est purus eu tempor tincidunt commodo.",
+    }
+
+]
+
+export default worksData;
