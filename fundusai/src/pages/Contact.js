d64bc3342cd5e8959120dc6ed77407e63db3ec6f
Sophia Edeki <sophiesse143@gmail.com>
2022-12-17 20:18:56 +0100

    updated contact page

--- fundusai/src/pages/Contact.js
+++ fundusai/src/pages/Contact.js
@@ -5,41 +5,43 @@ import call from "../images/vector1.svg";
 import email from "../images/Vector2.svg";
 import locate from "../images/Vector3.svg";
 import "../App.css"
-// import Ellipse from "../images/Ellipse.png"
-// import Ellipse2 from "../images/Ellipse2.png"
+import Ellipse from "../images/Ellipse.png"
+import Ellipse2 from "../images/Ellipse2.png"
 const Contact = () => {
   return (
     <section>
-      <div className="px-6 py-8 md:flex gap-2">
+      <div className="px-6 pt-6 pb-10 md:flex gap-2">
 
-    <div className="bg-[#044789] md:px-16 px-7 rounded-[14.44px] md:basis-5/12 h-[500px]">
-      <h3 className="text-white font-[poppins] font-semi-bold text-[30px] pt-6 pb-3 ">Stay connected with us</h3>
-      <p className="text-white font-[poppins] text-[15px]">We love to hear from you!</p>
-      <div className="py-16">
+    <div className="bg-[#044789] md:px-12 px-7 rounded-[14.44px]  md:max-w-[480px] h-[540px] relative">
+      <h3 className="text-white font-[poppins] font-semi-bold text-[30px] pt-8 pb-3 ">Stay connected with us</h3>
+      <p className="text-white font-[poppins] text-[18px]">We love to hear from you!</p>
+      <div className="py-20">
 
-      <div className="flex items-center  gap-4">
+      <div className="flex items-center py-3   gap-4">
         <img src={call} alt="call-icon" className="w-[15px]" />
-        <p className="text-white font-[poppins] text-[13px]">+234 9045 6789 62</p>
+        <p className="text-white font-[poppins] text-[20px]">+234 9045 6789 62</p>
       </div>
-      <div className="flex items-center  py-2 gap-4">
+      <div className="flex items-center py-4 gap-4">
         <img src={email} alt="email-icon" className="w-[15px]"/>
-        <p className="text-white font-[poppins] text-[13px]">support@fundusai.com</p>
+        <p className="text-white font-[poppins] text-[20px]">support@fundusai.com</p>
       </div>
-      <div className="flex items-center gap-4">
+      <div className="flex items-center py-3 gap-4">
         <img src={locate} alt="call-icon" className="w-[15px]"/>
-        <p className="text-white font-[poppins] text-[13px]">132 Moshood Street Asokoro, FCT 02156 Nigeria</p>
+        <p className="text-white font-[poppins] text-[20px]">132 Moshood Street Asokoro, FCT 02156 Nigeria</p>
       </div>
       </div>
-      <div className="flex items-center text-white pb-6  gap-4"> 
+      <div className="flex items-center text-white pt-6 pb-3  gap-4"> 
         <a href="/"><FiTwitter className="bg-black text-[18px] w-[30px] h-[30px] p-1 rounded-full"/></a>
         <a href="/"><BsInstagram className="bg-white text-black text-[18px] w-[30px] h-[30px] p-1 rounded-full" /></a>
         <a href="/"><TbBrandDiscord className="bg-black text-[18px] w-[30px] h-[30px] p-1 rounded-full"/></a>
       </div>
+      <img src={Ellipse} alt="a Circle" className="absolute bottom-14 right-20 w-[120px] " />
+      <img src={Ellipse2} alt="a ballon shape" className="absolute bottom-0 right-0 rounded-r-[10px]  w-[150px]" />
     </div>
     <div>
-      <form className="px-7 py-10 md:basis-7/12">
-        <div class="grid  grid-cols-2 grid-rows-2 ">
-
+      <form className="px-7 py-12 md:basis-1/2">
+        <div>
+        <div className="flex items-center gap-20">
         <div className=" pb-3">
         <label className="text-[#8d8d8d] font-base font-[poppins] text-[17px]">First Name</label><br/>
         <input type="text" className="border-b-[1.44px] border-[#8d8d8d] pt-3"/>
@@ -48,6 +50,8 @@ const Contact = () => {
         <label className="text-black font-base font-[poppins] text-[17px]">Last Name</label><br/>
         <input type="text" className="border-b-[1.44px] border-[#8d8d8d] pt-3"/>
         </div>
+        </div>
+        <div className="flex items-center gap-20">
         <div className="pt-5 ">
         <label className="text-[#8d8d8d] font-base font-[poppins] text-[17px]">Email</label><br/>
         <input type="email"className="border-b-[1.44px] border-[#8d8d8d] pt-3"/>
@@ -56,6 +60,8 @@ const Contact = () => {
         <label className="text-black font-base font-[poppins] text-[17px]">Phone Number</label><br/>
         <input type="tel"  pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" className="border-b-[1.44px] border-[#8d8d8d] pt-3"></input>
         </div>
+        </div>
+
         </div>
         <div className="pt-5 pb-3">
           <p className="text-[#0447b9] font-semi-bold font-[poppins] text-[18px] py-2">Select Subject?</p>
@@ -85,7 +91,6 @@ const Contact = () => {
         <div className="py-3 flex justify-end">
         <button type="submit" className="px-[60px] py-[20px] rounded-[7.22px] bg-[#ff9839] text-white text-[20px] font-base font-[poppins]">Send Message</button>
         </div>
-        
       </form>
     </div>
       </div>
