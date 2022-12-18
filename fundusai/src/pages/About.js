cdedd286cd3b7b845b1a333e85abb7c56c755649
DavidBolaji <preciouschiefofchrist@yahoo..com>
2022-12-18 19:24:54 +0100

    fixed contact page with formatting

--- fundusai/src/pages/About.js
+++ fundusai/src/pages/About.js
@@ -11,20 +11,32 @@ const About = () => {
 
   return (
     <section className="md:px-20 px-10 py-8">
-      <h1 className="font-[poppins] text-[#0447b9] text-[30px] md:text-[50] lg:text-[55px] font-semi-bold pb-3"> Get to know Us</h1>
+      <h1 className="font-[poppins] text-[#0447b9] text-[30px] md:text-[50] lg:text-[55px] font-semi-bold pb-3">
+        {" "}
+        Get to know Us
+      </h1>
       <div className="sm:flex  justify-between">
-
-      <p className="font-[poppins] font-light text-[15px] md:text-[20px] w-full py-3 lg:text-[30px] sm:max-w-[360px]  lg:max-w-[600px]">FundusAI offers an artificial intelligence diagnostic device for Diabetic Retinopathy, allowing doctors to diagnose more patients in less time in order to prevent blindness in diabetic patients.</p>
-      <div className="h-[230px] sm:w-[350px] sm:h-[200px]  lg:w-[500px] lg:h-[350px] bg-black flex justify-center items-center">
-        <span className="w-[100px] h-[100px] bg-[#7a8994] rounded-full flex justify-center items-center">
-          <a href="/" ><img src={play} alt="A play button" /></a>
-        </span>
-      </div>
+        <p className="font-[poppins] font-light text-[15px] md:text-[20px] w-full py-3 lg:text-[30px] sm:max-w-[360px]  lg:max-w-[600px]">
+          FundusAI offers an artificial intelligence diagnostic device for
+          Diabetic Retinopathy, allowing doctors to diagnose more patients in
+          less time in order to prevent blindness in diabetic patients.
+        </p>
+        <div className="h-[230px] sm:w-[350px] sm:h-[200px]  lg:w-[500px] lg:h-[350px] bg-black flex justify-center items-center">
+          <span className="w-[100px] h-[100px] bg-[#7a8994] rounded-full flex justify-center items-center">
+            <a href="/">
+              <img src={play} alt="A play button" />
+            </a>
+          </span>
+        </div>
       </div>
       <AboutMission />
       <div className="py-3">
-        <p className="font-[poppins] font-light text-[20px] md:text-[30px] text-[#0447b9]">MEET THE TEAM</p>
-        <p className="font-[poppins] font-base text-[38px] md:text-[50px] text-[#ff9839]">BEHIND THE TECHNOLOGY</p>
+        <p className="font-[poppins] font-light text-[20px] md:text-[30px] text-[#0447b9]">
+          MEET THE TEAM
+        </p>
+        <p className="font-[poppins] font-base text-[38px] md:text-[50px] text-[#ff9839]">
+          BEHIND THE TECHNOLOGY
+        </p>
       </div>
       {/* <AboutPageData /> */}
       {AboutCard}
